009091244b958e380fa5bf6897212fadb38a63da
Dariia Hurska <dariiahurska@gmail.com>
2025-05-06 16:25:02 +0300

    navigation done ✅

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,4 +1,4 @@
-import { Text, ImageBackground, Image } from "react-native";
+import {Text, ImageBackground, Image, View} from "react-native";
 import React from "react";
 import { Tabs } from "expo-router";
 import { images } from "@/constants/images";
@@ -9,7 +9,7 @@ const TabIcon = ({focused, icon, title }: any) => {
         return (
             <ImageBackground
                 source={images.highlight}
-                className="flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden"
+                className="flex flex-row w-full flex-1 min-w-[112px] min-h-16 mt-4 justify-center items-center rounded-full overflow-hidden"
             >
                 <Image
                     source={icon}
@@ -20,11 +20,36 @@ const TabIcon = ({focused, icon, title }: any) => {
             </ImageBackground>
         )
     }
+    return (
+        <View className="size-full justify-center items-center mt-4 rounded-full">
+            <Image source={icon} tintColor={"#A8B5DB"} className="size-5" />
+        </View>
+    )
 }
 
 const _Layout = () => {
     return (
-        <Tabs>
+        <Tabs
+            screenOptions={{
+                tabBarShowLabel: false,
+                tabBarItemStyle: {
+                    width: '100%',
+                    height: '100%',
+                    justifyContent: 'center',
+                    alignItems: 'center'
+                },
+                tabBarStyle:{
+                    backgroundColor: '#0f0D23',
+                    borderRadius: 60,
+                    marginHorizontal: 10,
+                    marginBottom: 36,
+                    height: 53,
+                    position: 'absolute',
+                    borderWidth: 1,
+                    borderColor: '#0f0D23',
+                }
+            }}
+        >
             <Tabs.Screen
                 name="index"
                 options={{
