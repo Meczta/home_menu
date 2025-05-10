2823ec55a65c05182be913cc344666f6a975ebd6
Dariia Hurska <dariiahurska@gmail.com>
2025-05-10 19:25:37 +0300

    move search page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -7,24 +7,24 @@ import {useRouter} from "expo-router";
 export default function Index() {
     const router = useRouter();
 
-  return (
-    <View className="flex-1 bg-primary">
-        <Image source={images.bg} className="absolute w-full h-full z-0" />
-        <ScrollView
-            className="flex-1 px-5"
-            showsVerticalScrollIndicator={false}
-            contentContainerStyle={{minHeight:"100%", paddingBottom: 10}}
-        >
-            <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto"/>
-
-            <View className="flex-1 mt-5">
-                <SearchBar
-                    onPress={() => router.push("/search")}
-                    placeholder="Search for a meal"
-                />
-            </View>
-
-        </ScrollView>
-    </View>
-  );
+  // return (
+  //   // <View className="flex-1 bg-primary">
+  //   //     <Image source={images.bg} className="absolute w-full h-full z-0" />
+  //   //     <ScrollView
+  //   //         className="flex-1 px-5"
+  //   //         showsVerticalScrollIndicator={false}
+  //   //         contentContainerStyle={{minHeight:"100%", paddingBottom: 10}}
+  //   //     >
+  //   //         <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto"/>
+  //   //
+  //   //         <View className="flex-1 mt-5">
+  //   //             <SearchBar
+  //   //                 onPress={() => router.push("/search")}
+  //   //                 placeholder="Search for a meal"
+  //   //             />
+  //   //         </View>
+  //   //
+  //   //     </ScrollView>
+  //   // </View>
+  // );
 }
--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,10 +1,31 @@
-import {View, Text} from "react-native";
+import {View, Text, Image, ScrollView} from "react-native";
 import React from "react";
+import {useRouter} from "expo-router";
+import {images} from "@/constants/images";
+import {icons} from "@/constants/icons";
+import SearchBar from "@/components/SearchBar";
 
 const Search = () => {
+    const router = useRouter();
+
     return (
-        <View>
-            <Text>Search</Text>
+        <View className="flex-1 bg-primary">
+            <Image source={images.bg} className="absolute w-full h-full z-0" />
+            <ScrollView
+                className="flex-1 px-5"
+                showsVerticalScrollIndicator={false}
+                contentContainerStyle={{minHeight:"100%", paddingBottom: 10}}
+            >
+                <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto"/>
+
+                <View className="flex-1 mt-5">
+                    <SearchBar
+                        // onPress={() => router.push("/search")}
+                        placeholder="Search for a meal"
+                    />
+                </View>
+
+            </ScrollView>
         </View>
     );
 }
